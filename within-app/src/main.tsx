b175dbc24c9d608a85409699207459eea07fcbff
thrw4c7n2t-lgtm <thrw4c7n2t@privaterelay.appleid.com>
2026-06-23 12:45:11 +1000

    Import workbook grammar fixes

--- within-app/src/main.tsx
+++ within-app/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './print-overrides.css'
+import './grammar-fixes.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
