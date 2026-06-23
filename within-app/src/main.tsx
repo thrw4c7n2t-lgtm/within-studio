c639b8061519d658e786cdae5b4c7d1cb7f9e3df
thrw4c7n2t-lgtm <thrw4c7n2t@privaterelay.appleid.com>
2026-06-23 13:30:10 +1000

    Remove workbook DOM enhancers

--- within-app/src/main.tsx
+++ within-app/src/main.tsx
@@ -2,13 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import './print-overrides.css'
-import './storybook-polish.css'
-import './front-cover.css'
-import './front-cover-fixes.css'
-import './front-cover-bold.css'
-import './grammar-fixes.ts'
-import './front-cover.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
