56af2d6c48fa5c600ee36008f064573ac283f85e
thrw4c7n2t-lgtm <thrw4c7n2t@privaterelay.appleid.com>
2026-06-23 15:29:46 +1000

    Load workbook polish styles after app styles

--- within-app/src/main.tsx
+++ within-app/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './template-pages.css'
 import App from './App.tsx'
+import './workbook-polish.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
